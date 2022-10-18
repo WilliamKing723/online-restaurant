d208f5b5b4f518a7e8f0ff58a3aaca06eed79900
WilliamKing723 <william72314@gmail.com>
2022-10-18 14:33:12 +0800

    Scrollspy

--- router/order.js
+++ router/order.js
@@ -7,6 +7,9 @@ router.get("/", function(req,res){
     res.send(fs.readFileSync('../Order/order.html').toString());
 });
 
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+})
 
 // moudle[1] 將router導出,等別人require引入使用
 module.exports = router ;
