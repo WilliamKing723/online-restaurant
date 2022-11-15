d2dd0fc373f708998fbc9cb66400487d05b02142
WilliamKing723 <william72314@gmail.com>
2022-11-15 20:24:02 +0800

    fixed problem

--- Order/order.js
+++ Order/order.js
@@ -1,7 +1,3 @@
-// (document).ready(function(){
-
-// })
-
 //backTop
 $(function(){
 	$('#BackTop').click(function(){ 
@@ -27,48 +23,59 @@ $(function(){
 			});
             $("#" + $(this).attr("id") + "Img").show();
             console.log("#" + $(this).attr("id") + "Img");
-            // if($(this).attr("id") === "salad") {
-            //     $("#saladImg").show();
-            // }
+            
 			$("#exampleModalLabel").text($(this).text());
 			$(".selectStyle").each(function(){
 				$(this).val(0).hide();
 			});
+            
+            $("#spicyChoose1").each(function(){
+				$(this).val(null);
+			});
 			switch($(this).text()){
 				case "炒麵": {
 					$("#bodylist_friedNoodles").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "湯麵": {
 					$("#bodylist_soupNoodles").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "乾麵": {
 					$("#bodylist_dryNoodles").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "炒泡麵": {
 					$("#bodylist_friedInstantNoodles").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "酸辣粉": {
 					$("#bodylist_powder").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "燴飯": {
 					$("#bodylist_risotto").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "炒飯": {
 					$("#bodylist_friedRice").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "咖哩飯": {
 					$("#bodylist_curryRice").show();
+                    $(".spicyCheck").show();
 					break;
 				}
 				case "丼飯": {
 					$("#bodylist_donburi").show();
+                    $(".spicyCheck").show();
 					break;
 				}
                 case "沙拉": {
@@ -303,4 +310,67 @@ $(function(){
     })
 });
 
+var shoplist = {}
+
+shoplist.name = "MyBuylist 購物清單"
+shoplist.time = "2020/12/30"
+shoplist.list = [
+
+]
+
+function showlist(){
+    $("#items_list").html("")
+    
+    let total_price = 0
+
+for(let i=0; i<shoplist.list.length; i++){
+    let item = shoplist.list[i]
+
+    let id = 0,num = 0,name = 0,price = 0,del_id = 0
+
+    id = `buyItem${i+1}`
+    num = i+1
+    name = item.name
+    price = item.price
+    del_id = `delItem${i+1}`
+    total_price += parseInt(item.price) 
+
+    let item_html=`<li id=${id} class="buy_item">${num}.${name}
+        <div class="price">${price}</div>
+        <div id=${del_id} class="del_btn">X</div>
+    </li>`
+
+
+
+    $("#items_list").append(item_html)
+    $(`#${del_id}`).click(function(){
+    removeItem(i)
+    })
+}
+
+let total = `<li class="buy_item total">總價
+    <div class="price">${total_price}</div>
+</li>`
+$("#items_list").append(total)
+}
+showlist()
+
+
+
+$(".addbtn").click(function(){
+shoplist.list.push({
+    name:$("#input_name").val(),
+    price:$("#input_price").val()
+})
+
+$("#input_name").val("")
+$("#input_price").val("")
+
+showlist()
+})
+
+function removeItem(id){
+shoplist.list.splice(id,1)
+showlist()
+}
 
